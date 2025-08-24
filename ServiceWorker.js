92f169777bbe459ef6d3b8b5196dc43c7709124c
naa000003 <152774827+naa000003@users.noreply.github.com>
2025-08-24 18:33:04 +0500

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "FPS-Slide Car Out-1.0";
+const contentToCache = [
+    "Build/Slide Car Out.loader.js",
+    "Build/Slide Car Out.framework.js.gz",
+    "Build/Slide Car Out.data.gz",
+    "Build/Slide Car Out.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
